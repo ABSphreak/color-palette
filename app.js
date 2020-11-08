a7ed371b4892e2d19873d100f5cc66ba046f7258
ABSphreak <absphreak@outlook.com>
2020-11-09 00:36:04 +0530

    ➕ Add color picking logic

---
+++ app.js
@@ -0,0 +1,12 @@
+// DOM Selections
+
+// Functions
+
+function generateHex() {
+  const letters = '#0123456789ABCDEF';
+  let hash = '#';
+  for (let i = 0; i < 6; i++) {
+    hash += letters[Math.floor(Math.random() * 16)];
+  }
+  return hash;
+}
