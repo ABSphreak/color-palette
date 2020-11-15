6c7ca9f5a52dfe3f316c2cf49c365f373419eb43
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2020-11-15 22:43:14 +0530

    ➕ Add selectors

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // DOM Selections
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelectorAll('.generate');
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll('.color h2');
 
 // Functions
 
+// Color Generator
 function generateHex() {
   const letters = '#0123456789ABCDEF';
   let hash = '#';
